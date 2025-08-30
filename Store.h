de305a29e36e6ad3ce5654de8016ca0a08d20cad
abraham21587 <abrahamwaggon821@gmail.com>
2025-08-30 09:28:49 -0500

    actualizo codigo

--- Store.h
+++ Store.h
@@ -6,29 +6,30 @@
 #define INC_4_CAL_TIENDA_VIDEOJ_ABRAHAM21587_STORE_H
 
 #include <string>
+using std::string;
 
 
 // Constantes
 const int MAX_PRODUCTOS = 12; // 4 categorias * 3 juegos
-const int MAX_CARRITO = 10; // limite maximo de items distintos en el carrito
+const int MAX_CARRITO = 10; // limite maximo de items en carrito
 
 
-// Prototipos de funciones (segun especificacion)
+// Prototipos de funciones (segun la especificacion)
 void mostrarCatalogoJuegos();
 int leerCodigoJuego();
 int leerCantidadJuegos();
-std::string obtenerNombreJuego(int codigoJuego);
+string obtenerNombreJuego(int codigoJuego);
 int obtenerPrecioJuego(int codigoJuego);
-std::string obtenerPlataforma(int codigoJuego);
-std::string obtenerCategoria(int codigoJuego);
-void agregarJuegoAlCarrito(int codigos[], int cantidades[], int& cantidadJuegosRegistrados);
+string obtenerPlataforma(int codigoJuego);
+string obtenerCategoria(int codigoJuego);
+void agregarJuegoAlCarrito(int codigos[], int cantidades[], int &cantidadJuegosRegistrados);
 double calcularSubtotalCarrito(const int codigos[], const int cantidades[], int cantidadJuegosRegistrados);
 int leerTipoCliente();
 double obtenerPorcentajeDescuento(int tipoCliente);
 double calcularDescuentosAdicionales(const int codigos[], const int cantidades[], int cantidadJuegosRegistrados);
 double calcularTotalFinal(double subtotal, double porcentajeDescuento, double descuentosAdicionales);
 void mostrarResumenCompra(const int codigos[], const int cantidades[], int cantidadJuegosRegistrados, double porcentajeDescuento);
-void cargarCompraDemo(int codigos[], int cantidades[], int& cantidadJuegosRegistrados);
-void limpiarCarrito(int codigos[], int cantidades[], int& cantidadJuegosRegistrados);
+void cargarCompraDemo(int codigos[], int cantidades[], int &cantidadJuegosRegistrados);
+void limpiarCarrito(int codigos[], int cantidades[], int &cantidadJuegosRegistrados);
 
 #endif //INC_4_CAL_TIENDA_VIDEOJ_ABRAHAM21587_STORE_H
